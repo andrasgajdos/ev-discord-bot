6545cff2299c634f940c213436296e8b679cb8e3
andrasgajdos <gajdosandras2003@gmail.com>
2025-10-29 17:01:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import functools
 import builtins
 import unicodedata
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from undetected_chromedriver import Chrome, ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from fake_useragent import UserAgent
+import random
 
 # Always flush print output
 print = functools.partial(builtins.print, flush=True)
